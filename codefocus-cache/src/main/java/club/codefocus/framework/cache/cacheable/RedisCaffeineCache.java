a92f8e4a2104205716aacd4cd92048f6106b4589
jackl <liudoudou@100tal.com>
2019-11-15 14:37:27 +0800

    fix:caffeineCache和redis同步

--- codefocus-cache/src/main/java/club/codefocus/framework/cache/cacheable/RedisCaffeineCache.java
+++ codefocus-cache/src/main/java/club/codefocus/framework/cache/cacheable/RedisCaffeineCache.java
@@ -16,7 +16,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
