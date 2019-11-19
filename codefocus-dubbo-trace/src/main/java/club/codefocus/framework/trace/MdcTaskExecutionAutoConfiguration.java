040f89719154a966bd194f2c9a982f8124602dc9
youdw <youdw@foxmail.com>
2019-11-19 14:05:50 +0800

    mdc task

--- codefocus-dubbo-trace/src/main/java/club/codefocus/framework/trace/MdcTaskExecutionAutoConfiguration.java
+++ codefocus-dubbo-trace/src/main/java/club/codefocus/framework/trace/MdcTaskExecutionAutoConfiguration.java
@@ -1,16 +1,23 @@
 package club.codefocus.framework.trace;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.task.TaskExecutionAutoConfiguration;
 import org.springframework.boot.autoconfigure.task.TaskExecutionProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.AsyncAnnotationBeanPostProcessor;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.Executor;
 import java.util.concurrent.ThreadPoolExecutor;
+
+import static org.springframework.boot.autoconfigure.task.TaskExecutionAutoConfiguration.APPLICATION_TASK_EXECUTOR_BEAN_NAME;
 
 /**
  * create at 2019/11/18 2:37 下午
@@ -20,16 +27,18 @@ import java.util.concurrent.ThreadPoolExecutor;
 @Configuration
 @EnableConfigurationProperties(TaskExecutionProperties.class)
 @Slf4j
+@AutoConfigureBefore(TaskExecutionAutoConfiguration.class)
 public class MdcTaskExecutionAutoConfiguration {
 
 
-    @Bean
+    @Bean(name = { APPLICATION_TASK_EXECUTOR_BEAN_NAME,
+            AsyncAnnotationBeanPostProcessor.DEFAULT_TASK_EXECUTOR_BEAN_NAME })
     @ConfigurationProperties(prefix = "spring.task.execution")
-    @ConditionalOnBean
-    public Executor asyncExecutor() {
-        log.info("mdcThreadPoolTaskExecutor init");
+    @ConditionalOnMissingBean(Executor.class)
+    public ThreadPoolTaskExecutor taskExecutor() {
+        log.debug("mdcThreadPoolTaskExecutor init");
         ThreadPoolTaskExecutor executor = new MdcThreadPoolTaskExecutor();
-        executor.setThreadNamePrefix("code-focus-executor-");
+        executor.setThreadNamePrefix("codefocus-executor-");
         executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
         executor.initialize();
         return executor;
