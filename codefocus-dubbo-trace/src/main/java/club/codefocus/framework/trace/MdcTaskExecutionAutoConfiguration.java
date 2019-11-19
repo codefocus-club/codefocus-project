d85b852d53f92895ad68fa301e27aa1a9d604c90
youdw <youdw@foxmail.com>
2019-11-19 13:00:53 +0800

    mdc task

--- codefocus-dubbo-trace/src/main/java/club/codefocus/framework/trace/MdcTaskExecutionAutoConfiguration.java
+++ codefocus-dubbo-trace/src/main/java/club/codefocus/framework/trace/MdcTaskExecutionAutoConfiguration.java
@@ -1,5 +1,7 @@
 package club.codefocus.framework.trace;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.task.TaskExecutionProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -17,12 +19,15 @@ import java.util.concurrent.ThreadPoolExecutor;
  */
 @Configuration
 @EnableConfigurationProperties(TaskExecutionProperties.class)
+@Slf4j
 public class MdcTaskExecutionAutoConfiguration {
 
 
     @Bean
     @ConfigurationProperties(prefix = "spring.task.execution")
+    @ConditionalOnBean
     public Executor asyncExecutor() {
+        log.info("mdcThreadPoolTaskExecutor init");
         ThreadPoolTaskExecutor executor = new MdcThreadPoolTaskExecutor();
         executor.setThreadNamePrefix("code-focus-executor-");
         executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
