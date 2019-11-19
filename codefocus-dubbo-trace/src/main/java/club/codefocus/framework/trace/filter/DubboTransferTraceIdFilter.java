77353c532ce5ae850a8646a8edb3c32a9f1a0083
youdw <youdw@foxmail.com>
2019-11-19 16:33:09 +0800

    fix: clear mdc

--- codefocus-dubbo-trace/src/main/java/club/codefocus/framework/trace/filter/DubboTransferTraceIdFilter.java
+++ codefocus-dubbo-trace/src/main/java/club/codefocus/framework/trace/filter/DubboTransferTraceIdFilter.java
@@ -19,16 +19,16 @@ public class DubboTransferTraceIdFilter implements Filter {
     private static final Logger LOGGER = LoggerFactory.getLogger(DubboTransferTraceIdFilter.class);
 
     @Override
-    public Result invoke(Invoker<?> invoker, Invocation invocation){
+    public Result invoke(Invoker<?> invoker, Invocation invocation) {
         RpcContext rpcContext = RpcContext.getContext();
-        Result result =null;
-        try{
+        Result result = null;
+        try {
             // traceId来源 web容器设置、非web容器
             String traceId = rpcContext.getAttachment(TraceConstant.TRACE_KEY);
             if (traceId == null) {
                 if (MDC.get(TraceConstant.TRACE_KEY) != null) {
                     traceId = MDC.get(TraceConstant.TRACE_KEY);
-                }else {
+                } else {
                     //重新生成
                     traceId = TraceIdUtil.generate();
                 }
@@ -42,15 +42,12 @@ public class DubboTransferTraceIdFilter implements Filter {
             MDC.put(TraceConstant.TRACE_KEY, traceId);
             result = invoker.invoke(invocation);
             if (rpcContext.isConsumerSide()) {
-                LOGGER.debug(" consumer :{}"+traceId);
+                LOGGER.debug(" consumer :{}" + traceId);
             }
-            if(rpcContext.isProviderSide()){
-                LOGGER.debug(" provider :{}"+traceId);
+            if (rpcContext.isProviderSide()) {
+                LOGGER.debug(" provider :{}" + traceId);
             }
-        }catch (Exception e){
-            e.printStackTrace();
-            LOGGER.error(e.getMessage());
-        } finally{
+        } finally {
             if (rpcContext.isProviderSide()) {
                 //provider端调用完毕收移除mdc值 server端保留
                 MDC.remove(TraceConstant.TRACE_KEY);
@@ -68,18 +65,21 @@ public class DubboTransferTraceIdFilter implements Filter {
         try {
             String traceId = rpcContext.getAttachment(TraceConstant.TRACE_KEY);
             if (rpcContext.isConsumerSide()) {
-                LOGGER.debug("onResponse  consumer :{}"+traceId);
+                LOGGER.debug("onResponse  consumer :{}" + traceId);
             }
-            if(rpcContext.isProviderSide()){
-                LOGGER.debug("onResponse provider :{}"+traceId);
+            if (rpcContext.isProviderSide()) {
+                LOGGER.debug("onResponse provider :{}" + traceId);
             }
-            return  result;
-        }finally {
-            //provider端调用完毕收移除mdc值 server端保留
-            MDC.remove(TraceConstant.TRACE_KEY);
-            if (LOGGER.isDebugEnabled()) {
-                LOGGER.debug("traceId clear");
+            return result;
+        } finally {
+            if (rpcContext.isProviderSide()) {
+                //provider端调用完毕收移除mdc值 server端保留
+                MDC.remove(TraceConstant.TRACE_KEY);
+                if (LOGGER.isDebugEnabled()) {
+                    LOGGER.debug("traceId clear");
+                }
             }
+
         }
     }
 }
