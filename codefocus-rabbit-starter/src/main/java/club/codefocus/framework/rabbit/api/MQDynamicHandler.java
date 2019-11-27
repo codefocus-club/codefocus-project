bf3a5d91645b4fdcc383e320b5635f873cdb11bb
youdw <youdw@foxmail.com>
2019-11-27 11:25:49 +0800

    fix: rabbit

--- codefocus-rabbit-starter/src/main/java/club/codefocus/framework/rabbit/api/MQDynamicHandler.java
+++ codefocus-rabbit-starter/src/main/java/club/codefocus/framework/rabbit/api/MQDynamicHandler.java
@@ -1,20 +1,22 @@
 package club.codefocus.framework.rabbit.api;
 
-import club.codefocus.framework.rabbit.config.ApplicationContextHelper;
 import club.codefocus.framework.rabbit.enums.EnumsExchangeType;
 import club.codefocus.framework.rabbit.factory.MQContainerFactory;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 /**
  * @author  jackl
  */
 @Component
-public class MQDynamicHandler {
-
+public class MQDynamicHandler implements ApplicationContextAware {
+    private static ApplicationContext applicationContext;
     @Autowired
     AmqpTemplate amqpTemplate;
 
@@ -24,8 +26,7 @@ public class MQDynamicHandler {
     @Autowired
     RabbitAdmin rabbitAdmin;
 
-    @Autowired
-    ApplicationContextHelper applicationContextHelper;
+
 
     public void publishMsg(String exchange, String routingKey, Object msg) {
         amqpTemplate.convertAndSend(exchange, routingKey, msg);
@@ -33,7 +34,7 @@ public class MQDynamicHandler {
 
     public void consumerGenerate(String beanName,String exchangeName, String queueName, String routingKey, boolean autoDelete, boolean durable,
                                  String acknowledgeMode, EnumsExchangeType exchangeType) throws Exception {
-        Object bean = applicationContextHelper.getBean(beanName);
+        Object bean = this.getBean(beanName);
         MQContainerFactory fac =
                 MQContainerFactory.builder().directExchange(exchangeName).queue(queueName).autoDeleted(autoDelete)
                         .acknowledgeMode(acknowledgeMode).durable(durable).routingKey(routingKey).rabbitAdmin(rabbitAdmin)
@@ -41,4 +42,11 @@ public class MQDynamicHandler {
         fac.getObject();
     }
 
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+    public Object getBean(String beanName) {
+        return applicationContext != null?applicationContext.getBean(beanName):null;
+    }
 }
